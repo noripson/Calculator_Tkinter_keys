2191903eda55681d292c2674afa88d87c8b757dc
noripson <77788778+noripson@users.noreply.github.com>
2024-10-15 19:48:24 +0330

    Update calculator_2D.py

--- calculator_2D.py
+++ calculator_2D.py
@@ -30,52 +30,38 @@ frame6.pack(side=TOP)
 # ==================Labels & Entry==========================
 result_display = Entry(frame1, font=('arial', 20, 'bold'), bd=10, justify='right').grid(columnspan=4)
 # =====================Buttons==============================
-button_number_7 = Button(frame3, width=8, height=5, text='7', bd=5)
-button_number_7.pack(side=LEFT, pady=2, padx=2)
+button_number_7 = Button(frame3, width=8, height=5, text='7', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_number_8 = Button(frame3, width=8, height=5, text='8', bd=5)
-button_number_8.pack(side=LEFT, pady=2, padx=2)
 
-button_number_9 = Button(frame3, width=8, height=5, text='9', bd=5)
-button_number_9.pack(side=LEFT, pady=2, padx=2)
+button_number_8 = Button(frame3, width=8, height=5, text='8', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_oprator_multiply = Button(frame3, width=8, height=5, text='x', bd=5)
-button_oprator_multiply.pack(side=LEFT, pady=2, padx=2)
+
+button_number_9 = Button(frame3, width=8, height=5, text='9', bd=5).pack(side=LEFT, pady=2, padx=2)
+
+button_oprator_multiply = Button(frame3, width=8, height=5, text='x', bd=5).pack(side=LEFT, pady=2, padx=2)
 # ------------------------------------------------------------
-button_number_4 = Button(frame4, width=8, height=5, text='4', bd=5)
-button_number_4.pack(side=LEFT, pady=2, padx=2)
+button_number_4 = Button(frame4, width=8, height=5, text='4', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_number_5 = Button(frame4, width=8, height=5, text='5', bd=5)
-button_number_5.pack(side=LEFT, pady=2, padx=2)
+button_number_5 = Button(frame4, width=8, height=5, text='5', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_number_6 = Button(frame4, width=8, height=5, text='6', bd=5)
-button_number_6.pack(side=LEFT, pady=2, padx=2)
+button_number_6 = Button(frame4, width=8, height=5, text='6', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_oprator_minus = Button(frame4, width=8, height=5, text='-', bd=5)
-button_oprator_minus.pack(side=LEFT, pady=2, padx=2)
+button_oprator_minus = Button(frame4, width=8, height=5, text='-', bd=5).pack(side=LEFT, pady=2, padx=2)
 # -----------------------------------------------------------
-button_number_1 = Button(frame5, width=8, height=5, text='1', bd=5)
-button_number_1.pack(side=LEFT, pady=2, padx=2)
+button_number_1 = Button(frame5, width=8, height=5, text='1', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_number_2 = Button(frame5, width=8, height=5, text='2', bd=5)
-button_number_2.pack(side=LEFT, pady=2, padx=2)
+button_number_2 = Button(frame5, width=8, height=5, text='2', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_number_3 = Button(frame5, width=8, height=5, text='3', bd=5)
-button_number_3.pack(side=LEFT, pady=2, padx=2)
+button_number_3 = Button(frame5, width=8, height=5, text='3', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_oprator_plus = Button(frame5, width=8, height=5, text='+', bd=5)
-button_oprator_plus.pack(side=LEFT, pady=2, padx=2)
+button_oprator_plus = Button(frame5, width=8, height=5, text='+', bd=5).pack(side=LEFT, pady=2, padx=2)
 # ---------------------------------------------------------
-button_oprator_remainder = Button(frame6, width=8, height=5, text='%', bd=5,)
-button_oprator_remainder.pack(side=LEFT, pady=2, padx=2)
+button_oprator_remainder = Button(frame6, width=8, height=5, text='%', bd=5,).pack(side=LEFT, pady=2, padx=2)
 
-button_number_0 = Button(frame6, width=8, height=5, text='0', bd=5)
-button_number_0.pack(side=LEFT, pady=2, padx=2)
+button_number_0 = Button(frame6, width=8, height=5, text='0', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_clear = Button(frame6, width=8, height=5, text='=', bd=5)
-button_clear.pack(side=LEFT, pady=2, padx=2)
+button_clear = Button(frame6, width=8, height=5, text='=', bd=5).pack(side=LEFT, pady=2, padx=2)
 
-button_oprator = Button(frame6, width=8, height=5, text='X', bd=5)
-button_oprator.pack(side=LEFT, pady=2, padx=2)
+button_oprator = Button(frame6, width=8, height=5, text='X', bd=5).pack(side=LEFT, pady=2, padx=2)
 # ==========================================================
 root.mainloop()
